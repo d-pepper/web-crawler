7f0c0e6343ba22bf4924b1b93b59a9434486f0a0
Dale Pepper <dale_pepper@hotmail.com>
2018-09-11 15:03:31 +0100

    Added test for parser class, and satisfied test

---
+++ WebCrawler.Tests/HtmlParserUnitTests.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace WebCrawler.Tests
+{
+    public class HtmlParserUnitTests
+    {
+        [Fact]
+        public void GetValidLinks_WhenHtmlStringIsPassed_ReturnsListOfInternalLinks()
+        {
+            // Arrange
+            const string htmlString = @"<html>
+                <body>
+                   
+                        <a href='http://www.internalsite.com/link1.html'>link 1</a>
+                        <a href='http://www.internalsite.com/link2.html'>link 2</a>
+                        <a href='http://www.internalsite.com/link3.html'>link 3</a>
+                        <a href='http://www.externalsite.com/link3.html'>link 4</a>
+                        <a href='http://www.externalsite.com/link3.html'>link 5</a>
+                   
+                </body>
+            </html>";
+
+            var htmlParser = new HtmlParser();
+            
+            // Act
+            var result = htmlParser.GetValidLinks("internalsite.com", htmlString);
+            
+            // Assert
+            result.Count().Should().Be(3);
+        }
+    }
+}
---
+++ WebCrawler/HtmlParser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace WebCrawler
+{
+    public class HtmlParser
+    {
+        public IEnumerable<string> GetValidLinks(string domain, string html)
+        {            
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var htmlBody = htmlDoc.DocumentNode.SelectNodes("//body");
+
+            var linkElements = htmlBody.Elements("a");
+
+            var links = new List<string>();
+
+            foreach (var node in linkElements)
+            {
+                if (node.NodeType == HtmlNodeType.Element)
+                {
+                    var href = node.Attributes.First(a => a.Name == "href");
+
+                    var link = href.Value;
+
+                    if (link.Contains(domain))
+                    {
+                        links.Add(href.Value);
+                    }          
+                }
+            }           
+
+            return links;
+        }
+    }
+}
